d2986be751a3fcda21753fff8a265f1ce7995b5b
rizkyfauziilmi <rizkyfauziilmi@gmail.com>
2025-07-29 21:43:51 +0700

    chore: comment out NEXT_PUBLIC_BASE_URL assignments in client environment configuration

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,12 +1,11 @@
 import { createEnv } from '@t3-oss/env-nextjs'
-import { z } from 'zod'
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_BASE_URL: process.env.NODE_ENV === 'development' ? z.url() : z.url().optional(),
+    // NEXT_PUBLIC_BASE_URL: process.env.NODE_ENV === 'development' ? z.url() : z.url().optional(),
   },
   runtimeEnv: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    // NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
   isServer: false,
   emptyStringAsUndefined: true,
